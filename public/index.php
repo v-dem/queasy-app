fd38c7e83efd82fa929286ed9f567a2b20e488d0
v-dem <vitaly_demyanenko@yahoo.com>
2017-05-28 20:11:16 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -9,10 +9,12 @@ define('QUEASY_PATH', dirname(dirname(INDEX_PATH)));
 // And we'll count that the vendor folder is in the main Queasy folder
 define('VENDOR_PATH', sprintf('%s%s%s', QUEASY_PATH, DIRECTORY_SEPARATOR, 'vendor'));
 
+define('QUEASY_CORE_PATH_TEMPLATE', '%s%s%sv-dem%squeasy-core%ssrc%sbootstrap.php');
+
 // Include Queasy bootstrapping file
 require_once(
     sprintf(
-        '%s%s%sv-dem%squeasy-core%ssrc%sbootstrap.php',
+        QUEASY_CORE_PATH_TEMPLATE,
         DIRECTORY_SEPARATOR, VENDOR_PATH, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR
     )
 );
