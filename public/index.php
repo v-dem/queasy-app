2e9f740617f66617ab2e768494c655d0d188d2e7
v-dem <vitaly_demyanenko@yahoo.com>
2017-05-28 19:11:29 +0300

    #12 #14 fix

--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
 <?php
 
-// Means that the parent folder is the main Queasy folder
-define('QUEASY_DIR', dirname(dirname(__FILE__)));
+// Set index.php path, do not change this
+define('INDEX_PATH', __FILE__);
 
-// Means that the parent folder is the main Queasy folder
-define('VENDOR_DIR', sprintf('%s%s%s', QUEASY_DIR, DIRECTORY_SEPARATOR, 'vendor'));
+// We'll count that the parent folder is the main Queasy path
+define('QUEASY_PATH', dirname(dirname(INDEX_PATH)));
+
+// And we'll count that the vendor folder is in the main Queasy folder
+define('VENDOR_PATH', sprintf('%s%s%s', QUEASY_PATH, DIRECTORY_SEPARATOR, 'vendor'));
 
 // Include Queasy bootstrapping file
-@require_once(
+require_once(
     sprintf(
-        '%s%sv-dem%squeasy-core%ssrc%sbootstrap.php',
-        DIRECTORY_SEPARATOR, VENDOR_DIR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR
+        '%s%s%sv-dem%squeasy-core%ssrc%sbootstrap.php',
+        DIRECTORY_SEPARATOR, VENDOR_PATH, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR
     )
 );
 
